f2fbf5519d99a0724de58238f8a4bdf66c5ae38a
James Bachrach <bachrachj@chc.edu>
2023-01-31 10:43:35 -0500

    January 31st

--- src/modules/CreateMenuItem/index.js
+++ src/modules/CreateMenuItem/index.js
@@ -1,10 +1,45 @@
-import { Card, Input, Button, InputNumber, Message, TextArea, Form } from "antd"; 
+import { Card, Input, Button, InputNumber, message, Form, } from "antd"; 
+
+const { TextArea } = Input;
 
 const CreateMenuItem = () => {
+    const onFinish = ({name, description, price}) => {
+        if (!name) {
+            message.error('Name Required!');
+            return;
+        }
+        if (!description) {
+            message.error('Description Required!');
+            return;
+        }
+        if (!price) {
+            message.error('Price Required!');
+            return;
+        }
+        message.success('Menu Item Created!');
+    };
+
     return (
         <Card title = {'Create New Item'} style = {styles.page}>
-            <Form layout = "vertical">
-
+            <Form layout = "vertical" onFinish = {onFinish}>
+                <Form.Item label = {'Name'} required name = {'name'}>
+                    <Input placeholder = "Enter Name"/>
+                </Form.Item>
+                <Form.Item label = {'Description'} required name = {'description'}>
+                    <TextArea 
+                        rows = {4}
+                        placeholder = {'Enter Desciption'}
+                    />
+                </Form.Item>
+                <Form.Item label = {'Price'} required name = {'price'}>
+                    <InputNumber placeholder = "Enter Price" />
+                </Form.Item>
+                <Form.Item label = {'Image'} name = {'image'}>
+                    <Input placeholder = "Enter Image Link" />
+                </Form.Item>
+                <Form.Item>
+                    <Button type = "primary" htmlType = "submit">Submit</Button>
+                </Form.Item>
             </Form>
         </Card>
     );
--- src/modules/CreateRestaurant/index.js
+++ src/modules/CreateRestaurant/index.js
@@ -1,7 +1,47 @@
+import { Card, Input, Button, message, Form, } from "antd"; 
+
 const CreateRestaurant = () => {
+
+    const onFinish = ({name, address, image}) => {
+        if (!name) {
+            message.error('Name required!');
+            return;
+        }
+        if (!address) {
+            message.error('Address required!');
+            return;
+        }
+        if (!image) {
+            message.error('Image required!');
+            return;
+        }
+        message.success('Restaurant Created!')
+    };
+
     return (
-        <div>Create Restaurant</div>
+        <Card title = {'Restaurant Details'} style = {styles.page}>
+            <Form layout = "vertical" onFinish = {onFinish}>
+                <Form.Item label = {'Name'} required name = {'name'}>
+                    <Input placeholder = "Enter Name" />
+                </Form.Item>
+                <Form.Item label = {'Address'} required name = {'address'}>
+                    <Input placeholder = "Enter Address" />
+                </Form.Item>
+                <Form.Item label = {'Image'} required name = {'image'}>
+                    <Input placeholder = "Enter Image Link"/>
+                </Form.Item>
+                <Form.Item>
+                    <Button type = "primary" htmlType = "submit">Submit</Button>
+                </Form.Item>
+            </Form>
+        </Card>
     );
+};
+
+const styles = {
+    page: {
+        margin: 20,
+    }
 };
 
 export default CreateRestaurant;
