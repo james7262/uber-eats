3676dca9ff388d90062ccc4ca167565c09e77b12
James Bachrach <bachrachj@chc.edu>
2023-02-21 10:44:09 -0500

    February 21st

--- src/modules/DetailedOrder/index.js
+++ src/modules/DetailedOrder/index.js
@@ -2,7 +2,7 @@ import { Card, Descriptions, Divider, List, Button, Tag, Spin } from 'antd';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { DataStore } from 'aws-amplify';
-import { Order, User, OrderDish, Dish } from '../../models';
+import { Order, User, OrderDish, Dish, OrderStatus } from '../../models';
 
 const statusToColor = {
     PENDING: 'blue',
@@ -59,6 +59,27 @@ const DetailedOrder = () => {
         fetchDishes();
     }, [orderDishes]);
 
+    const acceptOrder = async () => {
+        const updateOrder = await DataStore.save(Order.copyOf(order, updated => {
+            updated.status = OrderStatus.ACCEPTED;
+        }));
+        setOrder(updateOrder);
+    };
+
+    const declineOrder = async () => {
+        const updateOrder = await DataStore.save(Order.copyOf(order, updated => {
+            updated.status = OrderStatus.DECLINED;
+        }));
+        setOrder(updateOrder);
+    };
+
+    const foodIsDone = async () => {
+        const updateOrder = await DataStore.save(Order.copyOf(order, updated => {
+            updated.status = OrderStatus.COMPLETED;
+        }));
+        setOrder(updateOrder);
+    };
+
     if (!order) {
         return <Spin size = 'large'/>
     }
@@ -96,6 +117,7 @@ const DetailedOrder = () => {
                         type = 'primary'
                         size = 'large'
                         style = {styles.button}
+                        onClick = {declineOrder}
                     >
                         Decline Order
                     </Button>
@@ -104,6 +126,7 @@ const DetailedOrder = () => {
                         type = 'primary'
                         size = 'large'
                         style = {styles.button}
+                        onClick = {acceptOrder}
                     >
                         Accept Order
                     </Button>
@@ -112,6 +135,7 @@ const DetailedOrder = () => {
                         type = 'default'
                         size = 'large'
                         style = {styles.button}
+                        onClick = {foodIsDone}
                     >
                         Food is Done
                     </Button>
--- src/modules/RestaurantMenu/index.js
+++ src/modules/RestaurantMenu/index.js
@@ -1,8 +1,28 @@
-import { Card, Table, Button } from 'antd';
-import dishes from '../../data/dashboard/dishes.json';
+import { Card, Table, Button, message, Popconfirm } from 'antd';
+import { DataStore } from 'aws-amplify';
+import { useEffect, useState } from 'react';
+import { useRestaurantContext } from '../../context/RestaurantContext';
+import { Dish } from '../../models';
 import { Link } from "react-router-dom";
 
 const RestaurantMenu = () => {
+
+    const [dishes, setDishes] = useState([]);
+    const { restaurant } = useRestaurantContext();
+
+    useEffect(() => {
+        if (!restaurant?.id) {
+            return;
+        }
+        DataStore.query(Dish, d =>
+            d.restaurantID.eq(restaurant.id)).then(setDishes);
+    }, [restaurant?.id]);
+
+    const deleteDish = async (item) => {
+        await DataStore.delete(Dish, d => d.id.eq(item.id));
+        setDishes(dishes.filter((d) => d.id !== item.id));
+        message.success('Dish deleted!');
+    };
 
     const tableColumns = [
         {
@@ -14,12 +34,22 @@ const RestaurantMenu = () => {
             title: 'Price',
             dataIndex: 'price',
             key: 'price',
-            render: (price) => `$${price}`
+            render: (price) => `$${price.toFixed(2)}`
         },
         {
             title: 'Action',
             key: 'action',
-            render: () => <Button danger type = "primary"> Remove </Button>
+            render: (_, item) => (
+                <Popconfirm
+                    placement = "topLeft"
+                    title = {'Are you sure you want to delete this dish?'}
+                    onConfirm = {() => deleteDish(item)}
+                    okText = 'Yes'
+                    cancelText = 'No'
+                >
+                    <Button danger type = "primary"> Remove </Button>
+                </Popconfirm>
+            )
         }
     ];
 
